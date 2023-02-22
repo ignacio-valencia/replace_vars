1eca9cc734970b05e48e1f2a3e07eaf89b78fba3
Ignacio Valencia <86011934+ignacio-valencia@users.noreply.github.com>
2023-02-22 00:09:11 -0300

    github

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,15 @@ time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 gh version
 echo "The repository name is $GITHUB_REPOSITORY"
+
+
+
+export GH_HOST=GitHub.com
+
+export GH_ENTERPRISE_TOKEN=$GITHUB_TOKEN
+
+gh auth login
+
 gh api \
 -H "Accept: application/vnd.github+json" \
 /repos/$GITHUB_REPOSITORY/actions/variables
