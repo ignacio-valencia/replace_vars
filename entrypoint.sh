6ce18a9f26218ac41d78d8c2b46ecdb299d6f3c1
Ignacio Valencia <86011934+ignacio-valencia@users.noreply.github.com>
2023-02-21 23:38:10 -0300

    get vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,6 @@ time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 gh version
 echo "The repository name is $GITHUB_REPOSITORY"
+gh api \
+-H "Accept: application/vnd.github+json" \
+/repos/$GITHUB_REPOSITORY/actions/variables
