2465f55ed62a48de397e62f4dceafceda65672dd
Ignacio Valencia <86011934+ignacio-valencia@users.noreply.github.com>
2023-02-23 15:29:50 -0300

    fix token

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,11 @@ echo "The repository name is $GITHUB_REPOSITORY"
 
 export GH_HOST=GitHub.com
 
-gh auth login --with-token $GITHUB_TOKEN
+set -u
+echo "$GITHUB_TOKEN" > .githubtoken
+unset GITHUB_TOKEN
+gh auth login --with-token < .githubtoken
+rm .githubtoken
 
 gh api \
 -H "Accept: application/vnd.github+json" \
